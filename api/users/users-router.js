3db5eff655282178fe2db1cc4b2bc2b0127edab7
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-07-13 15:47:08 -0500

    delete functional

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -21,25 +21,23 @@ router.get('/:id', validateUserId, (req, res) => {
 });
 
 router.post('/', validateUser, (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
   Users.insert(req.body)
     .then(user => res.status(201).json(user))
     .catch(error => next(error))
 });
 
 router.put('/:id', validateUserId, validateUser, (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
   Users.update(req.user.id, req.body)
     .then(user => res.json(user))
     .catch(error => next(error))
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  Users.remove(req.user.id)
+    .then(res.json(req.user))
+    .catch(error => next(error))
 });
 
 router.get('/:id/posts', (req, res) => {
